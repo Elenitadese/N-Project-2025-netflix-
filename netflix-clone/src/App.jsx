a4bc34387089b980654f648f6a53720234bb9241
Elenitadese <elenitade1221@gmail.com>
2025-09-07 20:53:57 -0700

    Added API configuration (axios and requests) and a test file

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './Page/Home/Home'
+import TestFile from './Component/TestFile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
 <Home/>
+<TestFile/>
     
     </>
   )
---
+++ netflix-clone/src/Component/TestFile.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import requests from '../Utils/Requastes.jsx'
+
+const TestFile = () => {
+  useEffect(() => {
+    console.log(requests); // This will log the entire requests object
+  }, []);
+
+  return <div>...</div>;
+};
+export default TestFile;
