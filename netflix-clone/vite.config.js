49c66ba1e2d1b727caa3a9a9c0eb5e15f715375f
Elenitadese <elenitade1221@gmail.com>
2025-09-12 20:19:18 -0700

    repository name changed

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix_clone_2025/",
+  base: "/N-Project-2025-netflix-/",
 });
 
 
